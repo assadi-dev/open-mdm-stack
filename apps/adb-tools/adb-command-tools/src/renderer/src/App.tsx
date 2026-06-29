1227642bcc01ecad061eb7cf45daf080586ca16e
assadi-dev <assadi.halifa@gmail.com>
2026-06-29 23:55:55 +0200

    chore: install and update project dependencies in node_modules

---
+++ apps/adb-tools/adb-command-tools/src/renderer/src/App.tsx
@@ -0,0 +1,23 @@
+import AppFooter from "./components/AppFooter"
+import AppTitle from "./components/AppTitle"
+
+
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <div className="max-w-7xl mx-auto h-screen">
+      <div className=" flex flex-col justify-between items-center h-full w-full">
+        <div className="flex-1 p-5">
+          <AppTitle />
+
+        </div>
+        <div>
+          <AppFooter />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
