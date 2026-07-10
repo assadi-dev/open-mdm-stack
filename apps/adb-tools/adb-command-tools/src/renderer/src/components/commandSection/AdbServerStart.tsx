c54a52f61c81b47b0483b37569e307286e73da5d
assadi-dev <assadi.halifa@gmail.com>
2026-07-10 22:31:09 +0200

    feat: initialize adb-commands-tools Tauri project and add @tauri-apps/cli dependency

--- apps/adb-tools/adb-command-tools/src/renderer/src/components/commandSection/AdbServerStart.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function AdbServerStart(): React.JSX.Element {
-    return (
-        <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">Start ADB Server</p>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">Start Server</button>
-        </div>
-    )
-}
